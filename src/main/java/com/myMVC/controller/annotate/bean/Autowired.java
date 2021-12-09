42bd4bc7c05cc39f7d22e77086635789131f03cf
sherry-cup <1025868676@qq.com>
2021-12-09 23:37:05 +0800

    final code

--- src/main/java/com/myMVC/controller/annotate/bean/Autowired.java
+++ src/main/java/com/myMVC/controller/annotate/bean/Autowired.java
@@ -2,6 +2,8 @@ package com.myMVC.controller.annotate.bean;
 
 import java.lang.annotation.*;
 
+//通过Autowired注解实现beans的手动注入
+
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
